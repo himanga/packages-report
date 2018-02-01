0f02eedeca4e940ddc270f72937e3eda7448185e
himanga <30961866+himanga@users.noreply.github.com>
2018-02-01 13:37:31 -0600

    01_write-installed-packages

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,10 @@
 ## deja vu from yesterday!
+library(tidyverse)
+library(here)
+library(magrittr)
 
 ## create a data frame of your installed packages
+pkgs <- as_tibble(installed.packages())
 
 ## keep the variables
 ##   * Package
@@ -8,11 +12,13 @@
 ##   * Version
 ##   * Priority
 ##   * Built
+pkgs %<>% select(Package, LibPath, Version, Priority, Built)
 
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+write_csv(pkgs, path=here("data", "installed-packages.csv"))
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
